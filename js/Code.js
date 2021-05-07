d8c440068109474b1253405e83f8da8e971f51ea
omkar <78024859+omkar433434343@users.noreply.github.com>
2021-05-07 18:57:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,42 @@
+const accessCode1 = "variable";
+const accessCode2 = "function";
+const accessCode3 = "database";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("scrambled :- R E V B A I L A", 300,100)
+
+    fill("lightblue")
+    text("the start of a code", 100,70)
+
+    fill("cyan")
+    text("hint:- ", 60,70)
+    
+// second
+    fill("white")
+    textSize(20)
+    text("scrambled :- n o i t c n u f ", 300,220)
+
+    fill("lightblue")
+    text("the most important part of a code", 90,190)
+
+    fill("cyan")
+    text("hint:- ",40,190)
+//thrid 
+    fill("white") 
+    textSize(20)
+    text("scrambled :- a b s e d t a a  ", 300,330)
+
+    fill("lightblue")
+    text("the base to make your code a multiplayer ", 90,300)
+
+    fill("cyan")
+    text("hint:- ",40,300)  
+
+
+
+
+
+}
